167278d769a3f0e7a7dbc018088a3f75af07c26e
Helal Uddin <dmdhelal@gmail.com>
2024-01-08 18:17:00 +0600

    Menu Markup Setup With Wp Menu Object

--- template-parts/header-mega-menu.php
+++ template-parts/header-mega-menu.php
@@ -18,7 +18,6 @@ $logo_height = (!empty($logo_size) && is_numeric($logo_size[1]))
     ? $logo_size[1]
     : '43'; // 43 is the height of the default logo.
 ?>
-
 <header id="main-header" class="regicorp-mega-header" data-height-onload="<?php echo esc_attr(et_get_option('menu_height', '66')); ?>">
     <div class="clearfix et_menu_container">
         <div class="regicorp-header-nav">
@@ -33,92 +32,98 @@ $logo_height = (!empty($logo_size) && is_numeric($logo_size[1]))
                     <!-- navigation start -->
                     <nav role="navigation" id="regicorp-mega-nav" class="nav-menu-wrapper w-nav-menu">
                         <div class="nav-menu-content">
-                            <div class="nav-menu josh-test">
-                                <?php
-                                $menu_items_by_parent = array();
-                                foreach ($mega_menuitems as $menu_item) {
-                                    $menu_items_by_parent[$menu_item->menu_item_parent][] = $menu_item;
-                                }
-                                ?>
+                            <?php if ($mega_menuitems) { ?>
+                                <div class="nav-menu josh-test">
+                                    <?php
+                                    $menu_items_by_parent = array();
+                                    foreach ($mega_menuitems as $menu_item) {
+                                        $menu_items_by_parent[$menu_item->menu_item_parent][] = $menu_item;
+                                    }
+                                    ?>
 
-                                <?php foreach ($mega_menuitems as $menu_item) { ?>
+                                    <?php foreach ($mega_menuitems as $menu_item) { ?>
 
-                                    <?php if (isset($menu_items_by_parent[$menu_item->ID])) { ?>
-                                        <div class="nav-dropdown w-dropdown">
-                                            <div class="nav-dropdown-button w-dropdown-toggle" role="button">
-                                                <div class="nav-link"><?php echo esc_html($menu_item->title); ?></div>
-                                            </div>
-                                            <!-- dropdown sub menu items start -->
-                                            <?php
-                                            $submenu_items = $menu_items_by_parent[$menu_item->ID];
-                                            ?>
-                                            <nav class="nav-dropdown-list w-dropdown-list" id="w-dropdown-list-0">
-                                                <div class="nav-dropdown-inner">
-                                                    <div class="nav-dropdown-wrapper is-4-column">
-                                                        <?php foreach ($submenu_items as $submenu_item) {
-                                                            $shortDesc = get_post_meta($submenu_item->ID, '_menu_amq_short_desc', true);
-                                                            $sub_item_1 = get_post_meta($submenu_item->ID, '_amq_sub_menu_item_1', true);
-                                                            $sub_item_2 = get_post_meta($submenu_item->ID, '_amq_sub_menu_item_2', true);
-                                                            $sub_item_3 = get_post_meta($submenu_item->ID, '_amq_sub_menu_item_3', true);
-                                                            $sub_item_color = get_post_meta($submenu_item->ID, '_amq_menu_color', true);
-
-                                                            $card_color = $sub_item_color ? $sub_item_color : '#00f0ff';
-                                                        ?>
-                                                            <!-- cards start -->
-                                                            <div class="card-hover">
-                                                                <a href="<?php echo esc_url($submenu_item->url); ?>" class="regicorp-mega-card is-homepage is-1 w-inline-block" style="border-color: <?php echo $card_color; ?>; background-color: <?php echo $card_color; ?>;">
-                                                                    <div class="flex vertical align-left gap-s position-relative">
-                                                                        <div class="headline xs">
-                                                                            <?php echo esc_html($submenu_item->title); ?>
-                                                                        </div>
-
-                                                                        <div class="body-small line-height-125 balanced"><?php echo $shortDesc; ?></div>
-
-                                                                        <?php if ($sub_item_1) { ?>
-                                                                            <div class="nav-copilot-items">
-                                                                                <div class="pill-icon w-embed">
-                                                                                    <?php get_template_part('icons/campaign-svg'); ?>
-                                                                                </div>
-                                                                                <div class="headline xxs"><?php echo esc_html($sub_item_1); ?></div>
-                                                                            </div>
-                                                                        <?php } ?>
-
-                                                                        <?php if ($sub_item_2) { ?>
-                                                                            <div class="nav-copilot-items">
-                                                                                <div class="pill-icon w-embed">
-                                                                                    <?php get_template_part('icons/campaign-svg'); ?>
-                                                                                </div>
-                                                                                <div class="headline xxs"><?php echo esc_html($sub_item_2); ?></div>
-                                                                            </div>
-                                                                        <?php } ?>
-
-                                                                        <?php if ($sub_item_3) { ?>
-                                                                            <div class="nav-copilot-items">
-                                                                                <div class="pill-icon w-embed">
-                                                                                    <?php get_template_part('icons/campaign-svg'); ?>
-                                                                                </div>
-                                                                                <div class="headline xxs"><?php echo esc_html($sub_item_3); ?></div>
-                                                                            </div>
-                                                                        <?php } ?>
-
-                                                                    </div>
-                                                                    <div class="colorful-card-gradient pointer-none"></div>
-                                                                </a>
-                                                            </div>
-                                                            <!-- cards end -->
-                                                        <?php } ?>
-                                                    </div>
+                                        <?php if (isset($menu_items_by_parent[$menu_item->ID])) { ?>
+                                            <div class="nav-dropdown w-dropdown">
+                                                <div class="nav-dropdown-button w-dropdown-toggle" role="button">
+                                                    <div class="nav-link"><?php echo esc_html($menu_item->title); ?></div>
                                                 </div>
-                                            </nav>
-                                        </div>
-                                    <?php } elseif ($menu_item->menu_item_parent == 0) { ?>
-                                        <div class="nav-dropdown">
-                                            <a href="<?php echo esc_url($menu_item->url); ?>" class="nav-link w-nav-link"><?php echo esc_html($menu_item->title); ?></a>
-                                        </div>
-                                    <?php } ?>
+                                                <!-- dropdown sub menu items start -->
+                                                <?php
+                                                $submenu_items = $menu_items_by_parent[$menu_item->ID];
+                                                ?>
+                                                <nav class="nav-dropdown-list w-dropdown-list" id="w-dropdown-list-0">
+                                                    <div class="nav-dropdown-inner">
+                                                        <div class="nav-dropdown-wrapper is-4-column">
+                                                            <?php foreach ($submenu_items as $submenu_item) {
+                                                                $shortDesc = get_post_meta($submenu_item->ID, '_menu_amq_short_desc', true);
+                                                                $sub_item_1 = get_post_meta($submenu_item->ID, '_amq_sub_menu_item_1', true);
+                                                                $sub_item_2 = get_post_meta($submenu_item->ID, '_amq_sub_menu_item_2', true);
+                                                                $sub_item_3 = get_post_meta($submenu_item->ID, '_amq_sub_menu_item_3', true);
+                                                                $sub_item_color = get_post_meta($submenu_item->ID, '_amq_menu_color', true);
 
-                                <?php } ?>
-                            </div>
+                                                                $card_color = $sub_item_color ? $sub_item_color : '#00f0ff';
+                                                            ?>
+                                                                <!-- cards start -->
+                                                                <div class="card-hover">
+                                                                    <a href="<?php echo esc_url($submenu_item->url); ?>" class="regicorp-mega-card is-homepage is-1 w-inline-block" style="border-color: <?php echo $card_color; ?>; background-color: <?php echo $card_color; ?>;">
+                                                                        <div class="flex vertical align-left gap-s position-relative">
+                                                                            <div class="headline xs">
+                                                                                <?php echo esc_html($submenu_item->title); ?>
+                                                                            </div>
+
+                                                                            <div class="body-small line-height-125 balanced"><?php echo $shortDesc; ?></div>
+
+                                                                            <?php if ($sub_item_1) { ?>
+                                                                                <div class="nav-copilot-items">
+                                                                                    <div class="pill-icon w-embed">
+                                                                                        <?php get_template_part('icons/campaign-svg'); ?>
+                                                                                    </div>
+                                                                                    <div class="headline xxs"><?php echo esc_html($sub_item_1); ?></div>
+                                                                                </div>
+                                                                            <?php } ?>
+
+                                                                            <?php if ($sub_item_2) { ?>
+                                                                                <div class="nav-copilot-items">
+                                                                                    <div class="pill-icon w-embed">
+                                                                                        <?php get_template_part('icons/campaign-svg'); ?>
+                                                                                    </div>
+                                                                                    <div class="headline xxs"><?php echo esc_html($sub_item_2); ?></div>
+                                                                                </div>
+                                                                            <?php } ?>
+
+                                                                            <?php if ($sub_item_3) { ?>
+                                                                                <div class="nav-copilot-items">
+                                                                                    <div class="pill-icon w-embed">
+                                                                                        <?php get_template_part('icons/campaign-svg'); ?>
+                                                                                    </div>
+                                                                                    <div class="headline xxs"><?php echo esc_html($sub_item_3); ?></div>
+                                                                                </div>
+                                                                            <?php } ?>
+
+                                                                        </div>
+                                                                        <div class="colorful-card-gradient pointer-none"></div>
+                                                                    </a>
+                                                                </div>
+                                                                <!-- cards end -->
+                                                            <?php } ?>
+                                                        </div>
+                                                    </div>
+                                                </nav>
+                                            </div>
+                                        <?php } elseif ($menu_item->menu_item_parent == 0) { ?>
+                                            <div class="nav-dropdown">
+                                                <a href="<?php echo esc_url($menu_item->url); ?>" class="nav-link w-nav-link"><?php echo esc_html($menu_item->title); ?></a>
+                                            </div>
+                                        <?php } ?>
+
+                                    <?php } ?>
+                                </div>
+                            <?php } else {
+                            ?>
+                                <span>please select a menu to <a href="<?php echo admin_url('nav-menus.php'); ?>" target="_blank">Designed Mega Menu</a></span>
+                            <?php
+                            } ?>
                         </div>
                     </nav>
                     <!-- navigation end -->
